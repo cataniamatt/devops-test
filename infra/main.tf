6ec22e6a634ed17cdfe11dd5fd8811530321d4e5
Matthew Catania <matthewcatania@hotmail.com>
2024-03-04 22:17:06 +0100

    rename resource and change subscription

--- infra/main.tf
+++ infra/main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "rg" {
-  name = "rg-matthew-catania-${var.env}-${var.loc}"
+  name = "rg-mcatania-${var.env}-${var.loc}"
   location = var.location
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "acrmatthewcatania${var.env}${var.loc}"
+  name                = "acrmcatania${var.env}${var.loc}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
