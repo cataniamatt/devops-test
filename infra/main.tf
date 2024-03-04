d2f1b30a5c9cf13a2269cee2cde40a98614f225f
Matthew Catania <matthewcatania@hotmail.com>
2024-03-04 20:04:40 +0100

    created container registry code

--- infra/main.tf
+++ infra/main.tf
@@ -4,19 +4,8 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "containerRegistry1"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  sku                 = "Premium"
-  admin_enabled       = false
-  georeplications {
-    location                = "East US"
-    zone_redundancy_enabled = true
-    tags                    = {}
-  }
-  georeplications {
-    location                = "North Europe"
-    zone_redundancy_enabled = true
-    tags                    = {}
-  }
+  name                = "acrmatthewcatania${var.env}${var.loc}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
 }
