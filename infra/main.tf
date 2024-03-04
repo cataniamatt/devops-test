877be6d286d7a8c6c04199beca22b854a1b3686c
Matthew Catania <matthewcatania@hotmail.com>
2024-03-04 20:02:19 +0100

    rename rg resource block

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,22 @@
-resource "azurerm_resource_group" "rg_test" {
+resource "azurerm_resource_group" "rg" {
   name = "rg-matthew-catania-${var.env}-${var.loc}"
   location = var.location
 }
 
+resource "azurerm_container_registry" "acr" {
+  name                = "containerRegistry1"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "Premium"
+  admin_enabled       = false
+  georeplications {
+    location                = "East US"
+    zone_redundancy_enabled = true
+    tags                    = {}
+  }
+  georeplications {
+    location                = "North Europe"
+    zone_redundancy_enabled = true
+    tags                    = {}
+  }
+}
